df07b70d5cc488a085cfaa323ac05876bdc268f5
sudeepbhandari6545 <sudeepbhandari6545@gmail.com>
2021-05-06 14:21:58 +0545

    netflixchallange

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyCOxeGrVZJy7Kvrtwd-IAD2vSwDV8wlCSg',
+  authDomain: 'netflix-challenge-f39d9.firebaseapp.com',
+  projectId: 'netflix-challenge-f39d9',
+  storageBucket: 'netflix-challenge-f39d9.appspot.com',
+  messagingSenderId: '805234215147',
+  appId: '1:805234215147:web:014dfaa39ef0988f4a96ae',
+};
